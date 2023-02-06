f752a270317c9a0765681a8cc3490b9943532e6d
Sergio Lantigua <ingsergiolantigua@gmail.com>
2023-02-06 19:27:20 -0400

    firts commit

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace ACM.BL
+{
+    public class Customer
+    {
+
+        public string FirstName { get; set; }
+        public string EmailAddress { get; set; }
+        public int CustomerId { get; private set; }
+        public string FullName { get => $"{LastName} {FirstName}"; }
+
+        private string _lastName;
+        public string LastName { get => _lastName; set => _lastName = value; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Acme customer manager
