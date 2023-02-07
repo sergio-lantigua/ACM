bfddb2709753ef1075da7d9670360cef7cd0c607
Sergio Lantigua <ingsergiolantigua@gmail.com>
2023-02-07 09:56:16 -0400

    Add xUnit test project to the sln
    Add test suites for the Customer class of the BL

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -2,11 +2,26 @@
 {
     public class Customer
     {
-
+        //$"{LastName}, {FirstName}"
         public string FirstName { get; set; }
         public string EmailAddress { get; set; }
         public int CustomerId { get; private set; }
-        public string FullName { get => $"{LastName} {FirstName}"; }
+        public string FullName  
+        {
+            get 
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(LastName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName; 
+            }
+        }
 
         private string _lastName;
         public string LastName { get => _lastName; set => _lastName = value; }
---
+++ Test/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,70 @@
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggings"
+            };
+ 
+            string expected = "Baggings, Bilbo";
+
+
+            //-- Act
+            string actual = customer.FullName;
+
+
+            //-- Assert
+            Assert.Equal(expected, actual); 
+        }
+
+        [Fact]
+        public void FullName_FirstNameEmpty_ReturnLastName() {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Baggings"
+            };
+
+            string expected = "Baggings";
+
+
+            //-- Act
+            string actual = customer.FullName;
+
+
+            //-- Assert
+            Assert.Equal(expected, actual);
+
+        }
+        [Fact]
+        public void FullName_LastNameEmpty_ReturnFirstName()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo"
+            };
+
+            string expected = "Bilbo";
+
+
+            //-- Act
+            string actual = customer.FullName;
+
+
+            //-- Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+
+    }
+}
